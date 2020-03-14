b1bfefdacffb844b4f6d358c51447d785ac196e1
Fernanda Ramos <fernandaramos.a@outlook.com>
2020-03-14 01:34:13 -0300

    Fazer uma todo list com redux

---
+++ 05-lets-hooks-it-all/todo-list-challenge/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import AddTodo from './containers/AddTodo';
+import VisibleTodoList from './containers/VisibleTodoList';
+
+export default function App() {
+	return (
+		<div style={{ padding: 10 }}>
+			<AddTodo />
+			<VisibleTodoList />
+			<Footer />
+		</div>
+	);
+}
