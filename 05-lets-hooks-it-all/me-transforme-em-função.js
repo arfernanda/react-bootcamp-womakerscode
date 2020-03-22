28ea8c5b417a1e5afc53aea05a9f952df1bdce8a
Fernanda Ramos <fernandaramos.a@outlook.com>
2020-03-22 18:46:40 -0300

    Altera a indentação

--- 05-lets-hooks-it-all/me-transforme-em-função.js
+++ 05-lets-hooks-it-all/me-transforme-em-função.js
@@ -2,49 +2,49 @@ import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'semantic-react-ui';
 
 function ExampleComponent(porps) {
-	const [showForm, setShowForm] = useState(false);
-	const [email, setEmail] = useState('');
-	const [name, setName] = useState(props.name);
+  const [showForm, setShowForm] = useState(false);
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState(props.name);
 
-	useEffect(() => {
-		//
-	}, []);
+  useEffect(() => {
+  //
+  }, []);
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		//executa alguma request
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //executa alguma request
+  };
 
-	// RENDERS ------------------------------
+  // RENDERS ------------------------------
 
-	const renderForm = () => {
-		return (
-			<Form onSubmit={this.handleSubmit}>
-				<Form.Input name="name" value={name} onChange={(e) =>
-					setName(e.target.value)} />
-				<Form.Input name="email" value={email} onChange={(e) =>
-					setEmail(e.target.value)} />
-				<Form.Button type="submit">Submit!</Form.Button>
-			</Form>
-		);
-	};
+  const renderForm = () => {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Input name="name" value={name} onChange={(e) =>
+          setName(e.target.value)} />
+        <Form.Input name="email" value={email} onChange={(e) =>
+          setEmail(e.target.value)} />
+        <Form.Button type="submit">Submit!</Form.Button>
+      </Form>
+    );
+  };
 
-	// RENDER --------------------------------
+  // RENDER --------------------------------
 
-	return (
-		<div>
-			{showForm ? (
-				renderForm()
-			) : (
-					<Fragment>
-						<h3>Hi, {name}</h3>
-						<Button onClick={toggleShowForm}>
-							Enter Email Address!
-						</Button>
-					</Fragment>
-				)}
-		</div>
-	);
+  return (
+    <div>
+    {showForm ? (
+      renderForm()
+    ) : (
+      <Fragment>
+        <h3>Hi, {name}</h3>
+        <Button onClick={toggleShowForm}>
+          Enter Email Address!
+        </Button>
+      </Fragment>
+    )}
+    </div>
+  );
 }
 
 export default ExampleComponent;
